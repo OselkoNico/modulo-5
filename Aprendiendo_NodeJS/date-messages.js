4275f09ab33019f4e57b5dafe21127e0497e728f
Osel Nicolás <oselko123@gmail.com>
2026-04-07 10:40:34 +0200

    Aprendiendo npm

--- Aprendiendo_NodeJS/date-messages.js
+++ Aprendiendo_NodeJS/date-messages.js
@@ -8,8 +8,13 @@ const days = [
     'Domingo'
 ]
 
-const formatDate = date => '${days[(date.getDay()+6)%7]}, ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}';
+const formatDate = date => `${days[(date.getDay()+6)%7]}, ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
 
-const currentDateMessage = message => '${message} ${formatDate(new Date())}';
+const currentDateMessage = message => `${message} ${formatDate(new Date())}`;
 
-module.exports = currentDateMessage;
+const birthDateMessage = date => `Fecha de nacimiento: ${formatDate(new Date(date))}`;
+
+module.exports = {
+    currentDateMessage,
+    birthDateMessage
+};
