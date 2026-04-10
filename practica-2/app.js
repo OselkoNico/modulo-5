2c92832106d779fccf00f2e28aa7f168d5596fbd
Osel Nicolás <oselko123@gmail.com>
2026-04-10 12:03:00 +0200

    devolucion de archivos en express

--- practica-2/app.js
+++ practica-2/app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/respuesta-archivo', (req, res) => {
-    res.status(200).sendFile(__dirname + '/Resources/example.pdf');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(__dirname + '/public/index.html');
+})
+
+app.get('/contact', (req, res) => {
+    res.status(200).sendFile(__dirname + '/public/contact.html');
 })
 
 app.listen(port, () => {
