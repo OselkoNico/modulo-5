8189bab4fef6cf0ea6d11a961a218e9fd53cf478
Osel Nicolás <oselko123@gmail.com>
2026-04-10 10:50:03 +0200

    Seteo con express

--- practica-1/app.js
+++ practica-1/app.js
@@ -3,8 +3,20 @@ const app = express();
 const port = 3000;
 const logMessage = require('./middleware/logs');
 
-app.get('/', (req, res) => {
-    res.send('Hola mundo!');
+app.use((req, res, next) => {
+    res.set({'Set-Cookie': 'Test cookie'});
+    next();
+})
+
+app.get('/otra-ruta', (req, res) => {
+    res.status(200).send('Mensaje desde otra ruta');
+})
+
+app.get('/:id', (req, res) => {
+    if(req.params.id !== 'abc'){
+        return res.status(401).send('Acceso no autorizado.');
+    }
+    res.status(200).send('Hola mundo!');
 })
 app.listen(port, () => {
     console.log(`Servidor esuchando en http://localhost:${port}`);
